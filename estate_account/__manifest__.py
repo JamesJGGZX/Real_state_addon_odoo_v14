804e227da620e9942e93acd0678f08a786acc9b2
James <jamesblancozx@gmail.com>
2023-08-07 17:24:02 -0400

    [ADD] Se agrego un nuevo modulo a la carpeta addons_extends, el modulo estate_account el cual es un modulo de enlace entre el modulo estate y el modulo account de Odoo, tambien se agregaron nuevas vistas y modificaciones al modulo estate

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,7 +1,7 @@
 {
-    'name': 'Real Estate',
+    'name': 'Real Estate Account',
     'version': '14.0.1',
-    'category': 'sales, real estate',
+    'category': 'sales, real estate, account',
     'summary': 'This is an essay module of a real estate business model',
     'description': """
         In this module, a practice of the entire documentation course that is found within the main page of Odoo is carried out, 
@@ -9,15 +9,8 @@
     """,
     'author': 'Isaac Blanco',
     'website': 'None Bro',
-    'depends': [],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/estate_menus.xml',
-        'views/estate_property_views.xml',
-        'views/estate_property_type_views.xml',
-        'views/estate_property_offer_views.xml',
-        'views/inherited_model_users_views.xml'
-    ],
+    'depends': ['estate','account'],
+    'data': [],
     'demo': [
         #'demo/demo.xml',
     ],
