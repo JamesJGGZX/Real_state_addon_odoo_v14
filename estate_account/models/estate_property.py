3da40a564d873601d0b71088040496277eedbfb8
JamesJGGZX <jamesblancozx@gmail.com>
2023-08-08 00:56:38 -0400

    [ADD Se agrego un nuevo modulo llamado estate_account y una traducción al español, aun hay que hacer ciertas modificaciones en el modulo estate pero ya casi esta terminado (Hago de nuevo este comentario porque lo hice sin aplicar el git add antes XDDD)]

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,60 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _inherit = "real.estate"
+
+    def sold_property(self):
+        """Override the sold_property method to add custom logic"""
+    
+        res = super(EstateProperty, self).sold_property()
+    
+        # Obtener el partner_id y el move_type
+        partner_id = self.env.user.property_ids.buyer_id
+        move_type = 'out_invoice'
+    
+         # Verificar si el diario existe
+        # journal_name = 'Nombre del Diario'
+        # journal = self.env['account.journal'].search([('name', '=', journal_name)], limit=1)
+        
+        # if not journal:
+        #     raise ValueError(f"No se encontró el diario '{journal_name}'")
+        
+        # journal_id = journal.id
+    
+        # Crear valores para la factura vacía
+        move_values = {
+            'partner_id': partner_id,
+            'move_type': move_type,
+            # 'journal_id': journal_id,
+        }
+    
+        # Crear el objeto account.move vacío
+        new_move = self.env['account.move'].create(move_values)
+    
+        # Agregar líneas de factura
+        # Aqui se calcula el 6% del precio de venta
+        invoice_line_values = [
+            {
+                'name': 'Selling Price',
+                'quantity': 1,
+                'price_unit': self.selling_price * 0.06,
+                'move_id': new_move.id,
+            },
+        # Aqui se calculan las 100 tasas administrativas
+            {
+                'name': 'Administrative Fees',
+                'quantity': 1,
+                'price_unit': 100.00,
+                'move_id': new_move.id,
+            },
+        ]
+        
+        lines = [(0, 0, line_values) for line_values in invoice_line_values]
+        
+        new_move.write({
+            'invoice_line_ids': lines,
+        })
+    
+        # Aquí puedes agregar lógica personalizada de negocio
+    
+        return res
